81c8beb31052cf44af15ff5d7d3cf378b42cf7ce
Nancy Summer <nancy.summer@gmail.com>
2017-10-03 11:52:01 +0800

    rename package
    Signed-off-by: Nancy Summer <nancy.summer@gmail.com>

--- src/main/java/live/debug/msa/device/DemoApplication.java
+++ src/main/java/live/debug/msa/device/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@MapperScan
+//@MapperScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
