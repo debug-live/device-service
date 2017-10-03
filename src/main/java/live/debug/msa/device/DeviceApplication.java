88138db7a6f164afb1dc7cd830378d38fe8dd08b
Nancy Summer <nancy.summer@gmail.com>
2017-10-03 12:41:40 +0800

    mapperscan
    Signed-off-by: Nancy Summer <nancy.summer@gmail.com>

--- src/main/java/live/debug/msa/device/DeviceApplication.java
+++ src/main/java/live/debug/msa/device/DeviceApplication.java
@@ -1,14 +1,15 @@
 package live.debug.msa.device;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@MapperScan
-public class DemoApplication {
+@MapperScan("live.debug.msa.device.model.mapper")
+public class DeviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DeviceApplication.class, args);
 	}
 }
