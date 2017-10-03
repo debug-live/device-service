7f94ae5901e699f797c7d5b41a00b311d193de68
Conke Hu <conke.hu@gmail.com>
2017-10-03 11:33:52 +0800

    update
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- src/main/java/live/debug/msa/device/DeviceApplication.java
+++ src/main/java/live/debug/msa/device/DeviceApplication.java
@@ -3,7 +3,9 @@ package live.debug.msa.device;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("live.debug.msa.device.model.mapper")
 @SpringBootApplication
 public class DeviceApplication {
